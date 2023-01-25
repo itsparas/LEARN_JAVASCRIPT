95eb680f75d97b459a5b063d09884558b7c4668e
Paras Kumar <itskparas@gmail.com>
2023-01-25 17:53:49 +0530

    Day 3

--- asyncAndAwait.js
+++ asyncAndAwait.js
@@ -2,7 +2,7 @@
 
 const url = "https://api.github.com/users/itsparas";
 
-async function getUserDetai() {
+async function getUserDetail() {
   const user = await fetch(url);
   const userData = await user.json();
   console.log(userData);
