cc04600cb03d07635faa785f427ae633ff37cd84
Paras Kumar <itskparas@gmail.com>
2023-01-27 17:41:15 +0530

    day4

---
+++ practice.js
@@ -0,0 +1,67 @@
+// let n=8;
+// let str = "";
+// for(let i=0;i<n/2;i++){
+//     str ="";
+//     for(let j=0;j<=i;j++){
+//         str+=n/2-j;
+//     }
+//     for(let j=0;j<=n/2-2*i;j++){
+//         str+=n/2-i;
+//     }
+//     for(let k=0;k<i+1;k++){
+//         if(i-k==3){
+//             continue;
+//         }
+//         str+=n/2-i+k;
+//     }
+//     console.log(str);
+
+// }
+
+// for(let i=0;i<n/2-1;i++){
+//     str ="";
+//     for(let j=0;j<n/2-i-1;j++){
+//         str+=n/2-j;
+//     }
+//     for(let j=0;j<=2*i;j++){
+//         str+=i+2;
+//     }
+//     for(let k=0;k<n/2-1-i;k++){
+//         if(i-k==3){
+//             continue;
+//         }
+//         str+=n/2-2+k+i;
+//     }
+//     console.log(str);
+
+// }
+
+// for (let j = 0; j <= 4; j++) {
+//   process.stdout.write("*");
+// }
+
+// let arr = [1,2,3,4,1,2,3,4];
+
+// arr.sort();
+// let arr1 =[];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]!==arr[i+1]){
+//         arr1.push(arr[i]);
+//     }
+// }
+// console.log(arr1);
+
+// let arr = [1,2,3,4,5];
+
+// arr.sort();
+// let arr1 =[];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]!==arr[i+1]){
+//         arr1.push(arr[i]);
+//     }
+// }
+
+// let sum = arr.reduce((sum,value)=>{
+//     return sum+value;
+// },0);
+// console.log(sum);
